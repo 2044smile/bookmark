4c34079cbc86b0edefb8e1c474338d11b8297e19
2044smile <2044smile@naver.com>
2019-07-11 20:08:15 +0900

    add static_root

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6g&d!f66)k!$hc2sr@1pu(j+q&e_4cwmtb8&8hco(p4pzt2o+6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -107,5 +107,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
